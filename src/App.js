c9ec89c2fe5824f74b27af1cd4d01c39563b5156
xentoshi <xentoshi@protonmail.com>
2022-11-02 15:34:56 +0100

    Style updates

--- src/App.js
+++ src/App.js
@@ -9,14 +9,22 @@ function App() {
     <div className="App">
       {/*HEADER*/}
       <header className="App-header">
+      hi, you can call me xentoshi!
       </header>
       <div className="body">
       {/*ABOUT*/}
       <div className="about">
        </div>
-
+       <div className="about">
+        <li>political science graduate </li>
+        <li>self-taught js webdev</li>
+        <li>building <a href={laptopDAO} target="_blank" rel="noopener noreferrer" alt="laptopDAO"> laptopDAO</a></li>
+        <li>learning japanese</li>
+       </div>
+       <iframe src={gif} width="350" height="350" frameBorder="0" title="plant" className='gif' allowFullScreen></iframe>
        {/*LINKS*/}
       <div>
+      <a href={twitterHandle} target="_blank" rel="noopener noreferrer">twitter</a> | <a href ={github} target="_blank" rel="noopener noreferrer">github</a>
       </div>
        </div>
       </div>
