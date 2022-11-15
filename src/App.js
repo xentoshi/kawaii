4162b447c1ec32053c0e1fb1d3bacec77b7e65a9
xentoshi <xentoshi@protonmail.com>
2022-11-15 23:59:12 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-const laptopDAO = "https://laptop-dao.vercel.app/"
 const twitterHandle = "https://twitter.com/xentoshi"
 const github = "https://github.com/xentoshi"
 const gif = "https://giphy.com/embed/eg3fFwGU1TuwcKczgF"
@@ -9,18 +8,9 @@ function App() {
     <div className="App">
       {/*HEADER*/}
       <header className="App-header">
-      hi, you can call me xentoshi!
+      rebuilding... 
       </header>
       <div className="body">
-      {/*ABOUT*/}
-      <div className="about">
-       </div>
-       <div className="about">
-        <li>political science graduate </li>
-        <li>self-taught js webdev</li>
-        <li>building <a href={laptopDAO} target="_blank" rel="noopener noreferrer" alt="laptopDAO"> laptopDAO</a></li>
-        <li>learning japanese</li>
-       </div>
        <iframe src={gif} width="350" height="350" frameBorder="0" title="plant" className='gif' allowFullScreen></iframe>
        {/*LINKS*/}
       <div>
