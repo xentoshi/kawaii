97bcd75f1e257e5fca8e165396c4f4523a8b4f81
xentoshi <xentoshi@protonmail.com>
2023-04-02 18:13:43 +0900

    Not much changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import img from './img.jpeg'
-import resume from './resume.pdf'
 const twitterHandle = "https://twitter.com/xentoshi"
 const github = "https://github.com/xentoshi"
 const spotify = "https://open.spotify.com/playlist/4JbUa03GgHZrDisCMraXQg?si=5a80e3dfe356456f"
 const yourlittlekyoto = "https://www.instagram.com/yourlittlekyoto/"
 const laptopDAO = "https://www.laptopdao.org/"
-const linkedin = "https://www.linkedin.com/in/xentoshi/"
+
+const cute1 = "https://giphy.com/embed/oebD5alsVBFKg"
 
 
 function App() {
@@ -17,19 +16,18 @@ function App() {
         <p>Hi, my name is xentoshi and my ninja way is gentle, but persistent.</p>
       </header>
       <div className="body">
-      <p>Currently, I'm the founder of <a href ={laptopDAO} target="_blank" rel="noopener noreferrer">laptopDAO ⚡️</a>-- a nonprofit that is buying laptops to kids and a lil japanese dish shop -- <a href ={yourlittlekyoto} target="_blank" rel="noopener noreferrer">your little kyoto 🍣</a></p>
-      <p>When I like something -- I go after it, and I belive life is a wonderful adventure. While at it, may I meet you.</p>
+      <p>Currently, I'm the founder of <a href ={laptopDAO} target="_blank" rel="noopener noreferrer">laptopDAO ⚡️</a>-- a nonprofit that is buying laptops to kids and making mood board -- <a href ={yourlittlekyoto} target="_blank" rel="noopener noreferrer">@yourlittlekyoto</a>.</p>
+      <p>I believe life is a wonderful adventure. While at it, may I meet you. </p>
         {/*LINKS*/}
         <div>
-          <a href={twitterHandle} target="_blank" rel="noopener noreferrer"> twitter </a> |
-          <a href ={github} target="_blank" rel="noopener noreferrer"> github </a> |
-          <a href ={spotify} target="_blank" rel="noopener noreferrer"> spotify </a> |
-          <a href ={linkedin} target="_blank" rel="noopener noreferrer"> linkedin </a> |
-          <a href ={resume} target="_blank" rel="noopener noreferrer"> resume </a>
+          <a href={twitterHandle} target="_blank" rel="noopener noreferrer"> twitter </a> -
+          <a href ={github} target="_blank" rel="noopener noreferrer"> github </a> -
+          <a href ={spotify} target="_blank" rel="noopener noreferrer"> spotify </a> 
         </div>
+        <iframe src={cute1} width="150" height="150" frameBorder="0" title="cute1" className='cute1' allowFullScreen></iframe>
        </div>
        <div>
-          <img src={img} alt="meow" width={300} height={400} />
+         
         </div>
       </div>
   );
